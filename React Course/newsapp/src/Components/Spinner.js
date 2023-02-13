eaeab11c26d5e2d43249bebb725040a7673c659c
ShantanuSethReal <117711288+ShantanuSethReal@users.noreply.github.com>
2023-02-13 19:22:40 +0530

    News App

---
+++ React Course/newsapp/src/Components/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+export default class Spinner extends Component {
+  render() {
+    return (
+      <div className="d-flex justify-content-center">
+        <div className="spinner-border" role="status">
+          <span className="sr-only"></span>
+        </div>
+      </div>
+    );
+  }
+}
