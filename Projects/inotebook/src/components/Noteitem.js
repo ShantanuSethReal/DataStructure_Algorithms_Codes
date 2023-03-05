16444c184dd76cd5fba79e62d327a3e89ff10501
ShantanuSethReal <117711288+ShantanuSethReal@users.noreply.github.com>
2023-03-05 14:58:35 +0530

    Inotebook
    date time in note inserted

--- Projects/inotebook/src/components/Noteitem.js
+++ Projects/inotebook/src/components/Noteitem.js
@@ -5,13 +5,13 @@ const Noteitem = (props) => {
   const context = useContext(noteContext);
   const { deleteNote } = context;
   const { note, updateNote } = props;
-  // const d = new Date(note.date);
+  const d = new Date(note.date);
   return (
     <div className="col-md-3">
       <div className="card my-3">
-        {/* <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{ left: "90%", zIndex: "1" }}>
-        {note.tag}
-      </span> */}
+        <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{ left: "90%", zIndex: "1" }}>
+          {note.tag}
+        </span>
         <div className="card-body">
           <div className="d-flex align-items-center">
             <h5 className="card-title">{note.title}</h5>
@@ -29,7 +29,10 @@ const Noteitem = (props) => {
             ></i>
           </div>
           <p className="card-text">{note.description}</p>
-          {/* <div className="card-footer text-muted">Created on {d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear()}</div> */}
+          <div className="card-footer text-muted" style={{ textAlign: "center", fontSize: 12 }}>
+            Created on {d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear()}
+          </div>
+          <p style={{ textAlign: "center", fontSize: 9 }}>{d.toLocaleTimeString()}</p>
         </div>
       </div>
     </div>
