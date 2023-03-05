11db46bec61ad74888a3d73a43bf3c7cbcf5a6af
ShantanuSethReal <117711288+ShantanuSethReal@users.noreply.github.com>
2023-03-05 14:48:37 +0530

    iNotebook
    All working

---
+++ Projects/inotebook/src/components/Noteitem.js
@@ -0,0 +1,39 @@
+import React, { useContext } from "react";
+import noteContext from "../context/notes/noteContext";
+
+const Noteitem = (props) => {
+  const context = useContext(noteContext);
+  const { deleteNote } = context;
+  const { note, updateNote } = props;
+  // const d = new Date(note.date);
+  return (
+    <div className="col-md-3">
+      <div className="card my-3">
+        {/* <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{ left: "90%", zIndex: "1" }}>
+        {note.tag}
+      </span> */}
+        <div className="card-body">
+          <div className="d-flex align-items-center">
+            <h5 className="card-title">{note.title}</h5>
+            <i
+              className="far fa-trash-alt mx-2"
+              onClick={() => {
+                deleteNote(note._id);
+              }}
+            ></i>
+            <i
+              className="far fa-edit mx-2"
+              onClick={() => {
+                updateNote(note);
+              }}
+            ></i>
+          </div>
+          <p className="card-text">{note.description}</p>
+          {/* <div className="card-footer text-muted">Created on {d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear()}</div> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Noteitem;
---
+++ R1.java
@@ -0,0 +1,214 @@
+public class R1 {
+    class DP {
+        // *********************************************STRIVER DP
+        // PLAYLIST*********************************************** */
+        // *********************************************1D
+        // DP****************************************************************
+        // Climbing Stars (DP-2)
+        // Frog Jump(DP-3)
+        // Frog Jump with k distances(DP-4)
+        // Maximum sum of non-adjacent elements(DP-5)
+        // House Robber(DP-6)
+        // *********************************************2D and 3D DP and DP on
+        // Grids**********************************************
+        // Ninja’s Training(DP-7)
+        // Grid Unique Paths : DP on Grids(DP-8)
+        // Grid Unique Paths 2(DP-9)
+        // Minimum path sum in Grid(DP-10)
+        // Minimum path sum in Triangular Grid(DP-11)
+        // Minimum/Maximum Falling Path Sum(DP-12)
+        // 3-d DP : Ninja and his friends(DP-13)
+        // *********************************************DP on
+        // Subsequences********************************************************
+        // Subset sum equal to target(DP-14)
+        // Partition Equal Subset Sum(DP-15)
+        // Partition Set Into 2 Subsets With Min Absolute Sum Diff(DP-16)
+        // Count Subsets with Sum K(DP–17)
+        // Count Partitions with Given Difference(DP–18)
+        // 0/1 Knapsack(DP–19)
+        // Minimum Coins(DP–20)
+        // Target Sum(DP–21)
+        // Coin Change 2(DP–22)
+        // Unbounded Knapsack(DP–23)
+        // Rod Cutting Problem|(DP–24)
+        // ********************************************DP on
+        // Strings****************************************************************
+        // Longest Common Subsequence|(DP–25)
+        // Print Longest Common Subsequence|(DP–26)
+        // Longest Common Substring|(DP–27)
+        // Longest Palindromic Subsequence|(DP-28)
+        // Minimum insertions to make string palindrome|(DP-29)
+        // Minimum Insertions/Deletions to Convert String|(DP-30)
+        // Shortest Common Supersequence|(DP–31)
+        // Distinct Subsequences|(DP-32)
+        // Edit Distance|(DP-33)
+        // Wildcard Matching|(DP-34)
+        // *******************************************DP on
+        // Stocks******************************************************************
+        // Best Time to Buy and Sell Stock|(DP-35)
+        // Buy and Sell Stock–II|(DP-36)
+        // Buy and Sell Stocks III|(DP-37)
+        // Buy and Stock Sell IV|(DP-38)
+        // Buy and Sell Stocks With Cooldown|(DP-39)
+        // Buy and Sell Stocks With Transaction Fee|(DP-40)
+    }
+
+    class Stacks {
+        // ********************************************ACCIO MODULE
+        // 3*************************************************************** */
+        // Understanding stacks
+        // Extra Brackets
+        // Next Greater Element on Right
+        // Balanced Brackets
+        // Balanced Expression
+        // Previous Greater Element[NGE on Left]
+        // Stock Span Problem
+        // Largest Histogram Area
+        // Reverse Integer
+        // Trapping RainWater
+        // Merge Intervals
+        // Sum of Subarray Minimum**
+
+    }
+
+    class Sliding Window{
+        //********************************************ACCIO MODULE 3************************************************************
+        //Substring with K Unique characters
+        //Distinct Window
+        //Minimum Window Substring
+        //Longest substring without repeating characters(2 approaches)
+        //
+
+    }
+
+    class BinaryTree {
+        // ****************************************ACCIO-MODULE
+        // 3******************************************************************* */
+        // PreOrder,InOrder,PostOrder
+        // Size,Sum,Height Pre/Post Version
+        // Balanced Binary Tree
+        // Binary Tree-Level Order Traversal
+
+    }
+
+}
+
+    class Love_Babbar {
+        class Arrays {
+            // Reverse the array
+            // Find the maximum and minimum element in an array
+            // Find the “Kth” max and min element of an array
+            // Given an array which consists of only 0, 1 and 2. Sort the array without
+            // using any sorting algo
+            // Move all the negative elements to one side of the array
+            // Find the Union and Intersection of the two sorted arrays.
+            // Write a program to cyclically rotate an array by one.
+            // Find Largest sum contiguous Subarray [V. IMP]
+            // Minimize the maximum difference between heights [V.IMP]
+            // Minimum no. of Jumps to reach end of an array
+            // Find duplicate in an array of N+1 Integers
+            // Merge 2 sorted arrays without using Extra space.
+            // Kadane’s Algo [V.V.V.V.V IMP]
+            // Merge Intervals
+            // Next Permutation
+            // Count Inversion
+            // Best time to buy and Sell stock
+            // Find all pairs on integer array whose sum is equal to given number
+            // Find common elements In 3 sorted arrays
+            // Rearrange the array in alternating positive and negative items with O(1)
+            // extra space
+            // Find if there is any subarray with sum equal to 0
+            // Find factorial of a large number
+            // Find maximum product subarray
+            // Find longest consecutive subsequence
+            // Given an array of size n and a number k, fin all elements that appear more
+            // than ” n/k ” times. NA
+            // Maximum profit by buying and selling a share at most twice
+            // Find whether an array is a subset of another array
+            // Find the triplet that sum to a given value
+            // Trapping Rain water problem
+            // Chocolate Distribution problem
+            // Smallest Subarray with sum greater than a given value
+            // Three way partitioning of an array around a given value
+            // Minimum swaps required bring elements less equal K together
+            // Minimum no. of operations required to make an array palindrome
+            // Median of 2 sorted arrays of equal size
+            // Median of 2 sorted arrays of different size
+        }
+
+        class Matrix {
+            // Spiral traversal on a Matrix
+            // Search an element in a Matrix(Upper Right Corner method)
+            // Find median in a row wise sorted matrix
+            // Find row with maximum no. of 1’s
+            // Print elements in sorted order using row-column wise sorted matrix
+            // Maximum size rectangle
+            // Find a specific pair in matrix NA
+            // Rotate matrix by 90 degrees
+            // Kth smallest element in a row-column wise sorted matrix
+            // Common elements in all rows of a given matrix
+        }
+
+        class BackTracking {
+            // Rat in a maze Problem
+            // Print all possible paths from top left to bottom right of a mXn matrix
+            // Subset Sum Problem
+
+            // Printing all solutions in N-Queen
+            // Word Break Problem using Backtracking
+            // Remove Invalid Parentheses
+            // Sudoku Solver
+            // M Coloring Problem
+            // Print all palindromic partitions of a string
+
+            // The Knight’s tour problem
+            // Tug of War
+            // Find shortest safe route in a path with landmines
+            // Combinational Sum
+            // Find Maximum number possible by doing at-most K swaps
+            // Print all permutations of a string
+            // Find if there is a path of more than k length from a source
+            // Longest Possible Route in a Matrix with Hurdles
+
+            // Partition of a set into K subsets with equal sum
+            // Find the K-th Permutation Sequence of first N natural numbers
+        }
+
+        class Binary Tree{
+            Level order traversal	
+            Reverse Level Order traversal	
+            Height of a tree	
+            Diameter of a tree	
+            Mirror of a tree	
+            Inorder Traversal of a tree both using recursion and Iteration	
+            Preorder Traversal of a tree both using recursion and Iteration	
+            Postorder Traversal of a tree both using recursion and Iteration	
+            Left View of a tree	
+            Right View of Tree	
+            Top View of a tree	
+            Bottom View of a tree	
+            Zig-Zag traversal of a binary tree	
+            Check if a tree is balanced or not	
+            Diagonal Traversal of a Binary tree	
+            Boundary traversal of a Binary tree	
+            Construct Binary Tree from String with Bracket Representation	
+            Convert Binary tree into Doubly Linked List	
+            Convert Binary tree into Sum tree	
+            Construct Binary tree from Inorder and preorder traversal	
+            Find minimum swaps required to convert a Binary tree into BST	
+            Check if Binary tree is Sum tree or not	
+            Check if all leaf nodes are at same level or not	
+            Check if a Binary Tree contains duplicate subtrees of size 2 or more [ IMP ]	
+            Check if 2 trees are mirror or not	
+            Sum of Nodes on the Longest path from root to leaf node	
+            Check if given graph is tree or not.  [ IMP ]	
+            Find Largest subtree sum in a tree	
+            Maximum Sum of nodes in Binary tree such that no two are adjacent	
+            Print all “K” Sum paths in a Binary tree	
+            Find LCA in a Binary tree	
+            Find distance between 2 nodes in a Binary tree	
+            Kth Ancestor of node in a Binary tree	
+            Find all Duplicate subtrees in a Binary tree [ IMP ]	
+            Tree Isomorphism Problem	
+     }
+}
